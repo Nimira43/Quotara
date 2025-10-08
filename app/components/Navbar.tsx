03ef26b5ed0fb9280fe113443dccdacde9b166b9
Nimira43 <n.rabone72@aol.com>
2025-10-08 20:38:44 +0100

    Added is Active to NavLink elements for the nav menu items

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,44 +2,62 @@ import { NavLink } from 'react-router'
 import { BsChatSquareQuote } from "react-icons/bs";
 
 const Navbar = () => {
+  const base = 'nav-link hover-transition'
+  const active = 'text-orange-500 font-semibold'
+
   return ( 
     <nav className='bg-teal-800 shadow-md sticky top-0 z-50'>
       <div className='max-w-6xl mx-auto px-6 py-4 flex justify-between items-center'>
         <NavLink
           to='/'
-          className='flex items-center gap-2'
+          className='flex items-center gap-2 text-white text-3xl'
         >
-          <BsChatSquareQuote className='text-3xl text-white' />
-          <span className='text-3xl text-white logo pb-1'>Quotara</span>
+          <BsChatSquareQuote />
+          <span className='logo pb-1'>Quotara</span>
         </NavLink>
         <div className='hidden md:flex items-center gap-6'>
           <div className='space-x-4 text-sm text-white uppercase font-medium'>
             <NavLink 
-              className='nav-link hover-transition'
+              className={({ isActive }) => isActive
+                ? active
+                : base
+              }
               to='/'
             >
               Home
             </NavLink>
             <NavLink 
-              className='nav-link hover-transition'
+              className={({ isActive }) => isActive
+                ? active
+                : base
+              }
               to='/services'
             >
               Services
             </NavLink>
             <NavLink 
-              className='nav-link hover-transition'
+              className={({ isActive }) => isActive
+                ? active
+                : base
+              }
               to='/blog'
             >
               Blog
             </NavLink>
             <NavLink 
-              className='nav-link hover-transition'
+              className={({ isActive }) => isActive
+                ? active
+                : base
+              }
               to='/about'
             >
               About
             </NavLink>
             <NavLink 
-              className='nav-link hover-transition'
+              className={({ isActive }) => isActive
+                ? active
+                : base
+              }
               to='/contact'
             >
               Contact
