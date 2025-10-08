5a94598f6d175d4c4047f58d15c42729018c98ea
Nimira43 <n.rabone72@aol.com>
2025-10-08 18:07:52 +0100

    Created contact/index.tsx and added route

---
+++ app/routes/contact/index.tsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return ( 
+    <section>
+      <h2 className='text-3xl font-medium text-grey-dark mb-8 text-center'>Contact Us</h2>
+    </section>
+  )
+}
+ 
+export default Contact
