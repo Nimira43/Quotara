75d3f07b38a7bca8300931a0881d02dd9ff264cc
Nimira43 <n.rabone72@aol.com>
2025-10-22 18:26:55 +0100

    Header

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,13 @@
 import { Link } from 'react-router'
 
 const Hero = () => {
-  return (  )
+  return ( 
+    <header className='text-center py-20 px-4 transition-colors duration-300'>
+      <h2 className='text-4xl font-medium mb-4'> 
+        Welcome to Quotara
+      </h2>
+    </header>
+   )
 }
  
 export default Hero
