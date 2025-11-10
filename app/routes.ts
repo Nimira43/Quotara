894364c957c6ec2d3e73fa552fcaced9a469ba13
Nimira43 <n.rabone72@aol.com>
2025-11-10 17:12:41 +0000

    Moved index into layout array

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,10 @@
 import { type RouteConfig, index, route, layout } from '@react-router/dev/routes' 
 
 export default [
-  layout('./routes/layouts/home.tsx'), 
-  index('routes/home/index.tsx'),
+  layout('./routes/layouts/home.tsx', [
+    index('routes/home/index.tsx'),
+  ]), 
+  
   route('about', './routes/about/index.tsx'),
   route('contact', './routes/contact/index.tsx'),
   route('services', './routes/services/index.tsx'),
