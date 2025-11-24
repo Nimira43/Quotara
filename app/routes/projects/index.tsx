21b19ba9a42e42a198ceaf5ae65bc32734db1700
Nimira43 <n.rabone72@aol.com>
2025-11-24 01:24:43 +0000

    Updated correctly - Passing in Project type into loader

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,7 +1,7 @@
 import type { Project } from '~/types'
 import type { Route } from './+types/index'
 
-export async function loader({ request }: Route.LoaderArgs): Promise<Project> {
+export async function loader({ request }: Route.LoaderArgs): Promise<{projects: Project[]}> {
   const res = await fetch('http://localhost:8000/projects')
   const data = await res.json()
 
