daefce2da03a86ed9d0b781c720b1fc1dae63b58
Nimira43 <n.rabone72@aol.com>
2025-11-24 02:05:59 +0000

    ProjectCard - 2

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,14 +1,31 @@
 import { Link } from 'react-router'
 import type { Project } from '~/types'
 
-const ProjectCard = ({ projects}: {project: Project}) => {
+const ProjectCard = ({ project }: {project: Project}) => {
   return ( 
     <Link
       className='block transform transition transition-duration-300 hover:scale-[1.02]'
-      to={`/prodjects/${projects.id}`}
-    
+      to={`/prodjects/${project.id}`}
     >
-    
+      <div 
+        className='bg-teal-50 border border-orange-400 rounded-md overflow-hidden shadow-sm transitioning hover:shadow-md'
+      >
+        <img 
+          src={project.image} 
+          alt={project.title} 
+          className='w-full h-40 object-cover'
+        />
+        <div className='p-5'>
+          <h3 className='text-3xl font-medium text-teal-700 mb-1'>{project.title}</h3>
+          <p className='text-sm text-gray-900 mb-2'>
+            {project.description}
+          </p>
+          <div className='flex justify-between items-center text-sm text-orange-400 font-semibold uppercase'>
+            <span>{project.category}</span>
+            <span>{new Date(project.date).toLocaleDateString()}</span>
+          </div>
+        </div>
+      </div>
     </Link>
   )
 }
