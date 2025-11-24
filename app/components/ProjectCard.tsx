fcb21954679f6835531e01899ba0ee5b5c370226
Nimira43 <n.rabone72@aol.com>
2025-11-24 02:04:25 +0000

    ProjectCard - 1

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router'
 import type { Project } from '~/types'
 
 const ProjectCard = ({ projects}: {project: Project}) => {
   return ( 
-
+    <Link
+      className='block transform transition transition-duration-300 hover:scale-[1.02]'
+      to={`/prodjects/${projects.id}`}
+    
+    >
+    
+    </Link>
   )
 }
  
