3ac3145846bb4adced1843586de90ab5988c67c2
Nimira43 <n.rabone72@aol.com>
2025-12-09 20:46:34 +0000

    Params and type of return

--- app/routes/projects/details.tsx
+++ app/routes/projects/details.tsx
@@ -1,7 +1,12 @@
 import type { Route } from './+types'
 import type { Project } from '~/types'
 
-export async function ClientLoader() { }
+export async function ClientLoader({
+  request,
+  params
+}): Promise<Project> {
+  
+}
 
 const ProjectDetailsPage = () => {
   return ( 
