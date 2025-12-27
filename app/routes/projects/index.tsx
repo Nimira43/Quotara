2dcbc402d1ff3091715a002e1e3b397e0dd683a2
Nimira43 <n.rabone72@aol.com>
2025-12-27 15:15:57 +0000

    renderPagination - 4

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -22,13 +22,15 @@ const ProjectsPage = ({ loaderData}: Route.ComponentProps) => {
   const renderPagination = () => (
     <div className='flex justify-center gap-2 mt-8'>
       {Array.from({ length: totalPages }, (_, idx) => (
-        <button>
+        <button
           key={idx + 1}
           className={`px-3 py-1 cursor-pointer rounded ${
             currentPage === idx + 1 
             ? 'text-white border-2 border-orange-400 bg-orange-400'
             : 'text-orange-400 border-2 border-orange-400 bg-transparent'
-          }`}
+            }`}
+          onClick={() => setCurrentPage(idx + 1)}
+        >
         </button>
       ))}
     </div>
