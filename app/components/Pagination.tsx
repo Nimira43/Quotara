12f80fd09e5d5cc36bccd33d94387de43c7dae7b
Nimira43 <n.rabone72@aol.com>
2025-12-27 15:27:31 +0000

    Placed the JSX from renderPagination into PAgination.tsx

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -5,7 +5,23 @@ const Pagination = ({
 }) => {
   if (totalPages <= 1) return null 
 
-  return (  )
+  return ( 
+    <div className='flex justify-center gap-2 mt-8'>
+      {Array.from({ length: totalPages }, (_, idx) => (
+        <button
+          key={idx + 1}
+          className={`px-3 py-1 cursor-pointer rounded ${
+            currentPage === idx + 1 
+            ? 'text-white border-2 border-orange-400 bg-orange-400'
+            : 'text-orange-400 border-2 border-orange-400 bg-transparent'
+            }`}
+          onClick={() => setCurrentPage(idx + 1)}
+        >
+          { idx + 1 }
+        </button>
+      ))}
+    </div>
+  )
 }
  
 export default Pagination
