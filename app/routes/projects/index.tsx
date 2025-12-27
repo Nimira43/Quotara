9d2de6d2d2c8284913c973fa3602f20b80f9ddb3
Nimira43 <n.rabone72@aol.com>
2025-12-27 15:26:14 +0000

    Removed renderPagination function from index.tsx

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -19,23 +19,7 @@ const ProjectsPage = ({ loaderData}: Route.ComponentProps) => {
   const indexOfFirst = indexOfLast - projectsPerPage
   const currentProjects = projects.slice(indexOfFirst, indexOfLast)
 
-  const renderPagination = () => (
-    <div className='flex justify-center gap-2 mt-8'>
-      {Array.from({ length: totalPages }, (_, idx) => (
-        <button
-          key={idx + 1}
-          className={`px-3 py-1 cursor-pointer rounded ${
-            currentPage === idx + 1 
-            ? 'text-white border-2 border-orange-400 bg-orange-400'
-            : 'text-orange-400 border-2 border-orange-400 bg-transparent'
-            }`}
-          onClick={() => setCurrentPage(idx + 1)}
-        >
-          { idx + 1 }
-        </button>
-      ))}
-    </div>
-  )
+ 
 
   return ( 
     <>
